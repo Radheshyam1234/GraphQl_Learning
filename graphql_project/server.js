ec2323585e61f019d2687d5f878b2de6f9d4b7aa
Radheshyam1234 <rdk7352669258@gmail.com>
2023-12-17 19:02:00 +0530

    added queries and resolvers for getting all-users and all-quotes

--- graphql_project/server.js
+++ graphql_project/server.js
@@ -5,12 +5,27 @@ import { users, quotes } from "./fakedb.js";
 const typeDefs = gql`
   type Query {
     greet: String
+    users: [User]
+    quotes: [Quote]
+  }
+
+  type User {
+    id: ID
+    firstName: String
+    lastName: String
+    email: String
+  }
+  type Quote {
+    by: ID
+    name: String
   }
 `;
 
 const resolvers = {
   Query: {
     greet: () => "Hello Radheshyam",
+    users: () => users,
+    quotes: () => quotes,
   },
 };
 
